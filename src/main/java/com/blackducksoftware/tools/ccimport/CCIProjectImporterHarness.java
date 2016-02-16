f5c32345691714350293c66935a8128cd9cb06b1
stevebillings <sbillings@blackducksoftware.com>
2016-02-16 15:41:39 -0500

    5: Fixing a bug in reporting

--- src/main/java/com/blackducksoftware/tools/ccimport/CCIProjectImporterHarness.java
+++ src/main/java/com/blackducksoftware/tools/ccimport/CCIProjectImporterHarness.java
@@ -8,12 +8,12 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License version 2
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *******************************************************************************/
 package com.blackducksoftware.tools.ccimport;
 
@@ -123,7 +123,9 @@ public class CCIProjectImporterHarness {
                 processor.runReport();
             } else {
                 processor.performSynchronize();
-
+                if (processor instanceof CCISingleServerTaskProcessor) {
+                    System.out.println("\nSummary report: " + ((CCISingleServerTaskProcessor) (processor)).getAggregatedResults());
+                }
             }
 
             log.info("All finished.");
--- src/main/java/com/blackducksoftware/tools/ccimport/CCISingleServerTaskProcessor.java
+++ src/main/java/com/blackducksoftware/tools/ccimport/CCISingleServerTaskProcessor.java
@@ -8,12 +8,12 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License version 2
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *******************************************************************************/
 
 /**
@@ -43,10 +43,10 @@ import com.blackducksoftware.tools.ccimporter.config.CodeCenterConfigManager;
 import com.blackducksoftware.tools.ccimporter.config.ProtexConfigManager;
 import com.blackducksoftware.tools.ccimporter.model.CCIProject;
 import com.blackducksoftware.tools.ccimporter.model.CCIProjectList;
-import com.blackducksoftware.tools.connector.protex.ProtexServerWrapper;
 import com.blackducksoftware.tools.commonframework.core.config.server.ServerBean;
-import com.blackducksoftware.tools.connector.codecenter.CodeCenterServerWrapper;
 import com.blackducksoftware.tools.commonframework.standard.protex.ProtexProjectPojo;
+import com.blackducksoftware.tools.connector.codecenter.CodeCenterServerWrapper;
+import com.blackducksoftware.tools.connector.protex.ProtexServerWrapper;
 
 /**
  * Synchronizes a single Protex server with a Code Center server using a
@@ -59,15 +59,18 @@ import com.blackducksoftware.tools.commonframework.standard.protex.ProtexProject
 public class CCISingleServerTaskProcessor extends CCIProcessor {
 
     private static final Logger log = LoggerFactory
-	    .getLogger(CCISingleServerTaskProcessor.class.getName());
+            .getLogger(CCISingleServerTaskProcessor.class.getName());
 
     private final int numThreads;
+
     private final ProtexConfigManager protexConfigManager;
+
     private final ProtexServerWrapper<ProtexProjectPojo> protexServerWrapper;
 
     private final SyncProjectTaskFactory taskFactory;
 
     private CCIReportSummary aggregatedResults;
+
     private StringBuilder threadExceptionMessages = new StringBuilder();
 
     /**
@@ -76,21 +79,21 @@ public class CCISingleServerTaskProcessor extends CCIProcessor {
      * @throws Exception
      */
     public CCISingleServerTaskProcessor(
-	    CodeCenterConfigManager ccConfigManager,
-	    ProtexConfigManager protexConfigManager,
-	    CodeCenterServerWrapper codeCenterServerWrapper,
-	    ProtexServerWrapper<ProtexProjectPojo> protexServerWrapper,
-	    SyncProjectTaskFactory taskFactory) throws Exception {
+            CodeCenterConfigManager ccConfigManager,
+            ProtexConfigManager protexConfigManager,
+            CodeCenterServerWrapper codeCenterServerWrapper,
+            ProtexServerWrapper<ProtexProjectPojo> protexServerWrapper,
+            SyncProjectTaskFactory taskFactory) throws Exception {
 
-	super(ccConfigManager, codeCenterServerWrapper);
-	this.protexConfigManager = protexConfigManager;
-	this.protexServerWrapper = protexServerWrapper;
-	this.taskFactory = taskFactory;
-	numThreads = ccConfigManager.getNumThreads();
+        super(ccConfigManager, codeCenterServerWrapper);
+        this.protexConfigManager = protexConfigManager;
+        this.protexServerWrapper = protexServerWrapper;
+        this.taskFactory = taskFactory;
+        numThreads = ccConfigManager.getNumThreads();
 
-	// There will only be one in the single instance
-	ServerBean protexBean = protexConfigManager.getServerBean();
-	log.info("Using Protex URL [{}]", protexBean.getServerName());
+        // There will only be one in the single instance
+        ServerBean protexBean = protexConfigManager.getServerBean();
+        log.info("Using Protex URL [{}]", protexBean.getServerName());
     }
 
     /**
@@ -98,118 +101,118 @@ public class CCISingleServerTaskProcessor extends CCIProcessor {
      */
     @Override
     public void performSynchronize() throws CodeCenterImportException {
-	ExecutorService exec = Executors.newFixedThreadPool(numThreads);
-	boolean threadExceptionThrown = false;
-	try {
-	    CompletionService<CCIReportSummary> completionService = new ExecutorCompletionService<>(
-		    exec);
-	    List<CCIProject> projectList = getProjects().getList();
+        ExecutorService exec = Executors.newFixedThreadPool(numThreads);
+        boolean threadExceptionThrown = false;
+        try {
+            CompletionService<CCIReportSummary> completionService = new ExecutorCompletionService<>(
+                    exec);
+            List<CCIProject> projectList = getProjects().getList();
 
-	    log.info("Processing {} projects for synchronization", projectList);
-	    if (projectList.size() == 0) {
-		throw new CodeCenterImportException(
-			"No valid projects were specified.");
-	    }
+            log.info("Processing {} projects for synchronization", projectList);
+            if (projectList.size() == 0) {
+                throw new CodeCenterImportException(
+                        "No valid projects were specified.");
+            }
 
-	    aggregatedResults = new CCIReportSummary();
-	    int numProjectsSubmitted = submitTasks(completionService,
-		    projectList, aggregatedResults);
+            aggregatedResults = new CCIReportSummary();
+            int numProjectsSubmitted = submitTasks(completionService,
+                    projectList, aggregatedResults);
 
-	    // Collect results from tasks as they finish
+            // Collect results from tasks as they finish
 
-	    for (int taskNum = 0; taskNum < numProjectsSubmitted; taskNum++) {
-		Future<CCIReportSummary> f = null;
-		CCIReportSummary singleTaskResult = null;
-		try {
-		    f = completionService.take();
-		    singleTaskResult = f.get();
-		} catch (InterruptedException | ExecutionException e) {
-		    String appName = getAppNameFromException(e);
-		    String msg = composeErrorMessage(appName, e);
-		    log.error(msg, e);
-		    threadExceptionThrown = true;
-		    threadExceptionMessages.append("; ");
-		    threadExceptionMessages.append(msg);
-		    singleTaskResult = generateErrorTaskResults(appName);
-		}
+            for (int taskNum = 0; taskNum < numProjectsSubmitted; taskNum++) {
+                Future<CCIReportSummary> f = null;
+                CCIReportSummary singleTaskResult = null;
+                try {
+                    f = completionService.take();
+                    singleTaskResult = f.get();
+                } catch (InterruptedException | ExecutionException e) {
+                    String appName = getAppNameFromException(e);
+                    String msg = composeErrorMessage(appName, e);
+                    log.error(msg, e);
+                    threadExceptionThrown = true;
+                    threadExceptionMessages.append("; ");
+                    threadExceptionMessages.append(msg);
+                    singleTaskResult = generateErrorTaskResults(appName);
+                }
 
-		System.out.println("Got result " + taskNum + " of "
-			+ numProjectsSubmitted + ": " + singleTaskResult);
+                System.out.println("Got result " + (taskNum + 1) + " of "
+                        + numProjectsSubmitted + ": " + singleTaskResult);
 
-		aggregatedResults.addReportSummary(singleTaskResult);
-	    }
-	} finally {
-	    exec.shutdown();
-	}
-	if (threadExceptionThrown) {
-	    log.error("Exception(s) thrown from worker thread(s): "
-		    + getThreadExceptionMessages());
-	} else {
-	    log.info("All threads finished.");
-	}
+                aggregatedResults.addReportSummary(singleTaskResult);
+            }
+        } finally {
+            exec.shutdown();
+        }
+        if (threadExceptionThrown) {
+            log.error("Exception(s) thrown from worker thread(s): "
+                    + getThreadExceptionMessages());
+        } else {
+            log.info("All threads finished.");
+        }
     }
 
     private String composeErrorMessage(String appName, Exception e) {
-	StringBuilder sb = new StringBuilder("Error in task for app ");
-	sb.append(appName);
-	sb.append(": ");
-	String eMessage = e.getMessage();
-	sb.append(eMessage);
-	String causeMessage = e.getCause().getMessage();
-	if ((eMessage != null) && (e.getCause() != null)
-		&& (!eMessage.contains(causeMessage))) {
-	    sb.append(" (");
-	    sb.append(causeMessage);
-	    sb.append(")");
-	}
-	return sb.toString();
+        StringBuilder sb = new StringBuilder("Error in task for app ");
+        sb.append(appName);
+        sb.append(": ");
+        String eMessage = e.getMessage();
+        sb.append(eMessage);
+        String causeMessage = e.getCause().getMessage();
+        if ((eMessage != null) && (e.getCause() != null)
+                && (!eMessage.contains(causeMessage))) {
+            sb.append(" (");
+            sb.append(causeMessage);
+            sb.append(")");
+        }
+        return sb.toString();
     }
 
     private CCIReportSummary generateErrorTaskResults(String appName) {
-	CCIReportSummary singleTaskResult = new CCIReportSummary();
-	singleTaskResult.setTotalCCApplications(1);
-	singleTaskResult.setTotalProtexProjects(1);
-	singleTaskResult.addTotalImportsFailed();
-	singleTaskResult.addToFailedImportList(appName);
-	return singleTaskResult;
+        CCIReportSummary singleTaskResult = new CCIReportSummary();
+        singleTaskResult.setTotalCCApplications(1);
+        singleTaskResult.setTotalProtexProjects(1);
+        singleTaskResult.addTotalImportsFailed();
+        singleTaskResult.addToFailedImportList(appName);
+        return singleTaskResult;
     }
 
     private String getAppNameFromException(Exception e) {
-	String appName = "<unknown>";
-	if (e instanceof ExecutionException) {
-	    if (e.getCause() instanceof CodeCenterImportNamedException) {
-		appName = ((CodeCenterImportNamedException) e.getCause())
-			.getAppName();
-	    }
-	}
-	return appName;
+        String appName = "<unknown>";
+        if (e instanceof ExecutionException) {
+            if (e.getCause() instanceof CodeCenterImportNamedException) {
+                appName = ((CodeCenterImportNamedException) e.getCause())
+                        .getAppName();
+            }
+        }
+        return appName;
     }
 
     private int submitTasks(
-	    CompletionService<CCIReportSummary> completionService,
-	    List<CCIProject> projectList, CCIReportSummary aggregatedResults) {
-	int numSubmitted = 0;
-	for (CCIProject project : projectList) {
-	    Pattern projectNameFilterPattern = protexConfigManager
-		    .getProtexProjectNameFilterPattern();
-	    if (projectNameFilterPattern != null) {
-		Matcher m = projectNameFilterPattern.matcher(project
-			.getProjectName());
-		if (!m.matches()) {
-		    log.info(
-			    "Project {} does not match the project name filter; skipping it",
-			    project.getProjectName());
-		    CCIReportSummary skippedProjectResult = new CCIReportSummary();
-		    skippedProjectResult.addTotalProjectsSkipped();
-		    aggregatedResults.addReportSummary(skippedProjectResult);
-		    continue;
-		}
-	    }
-	    numSubmitted++;
-	    Callable<CCIReportSummary> task = taskFactory.createTask(project);
-	    completionService.submit(task);
-	}
-	return numSubmitted;
+            CompletionService<CCIReportSummary> completionService,
+            List<CCIProject> projectList, CCIReportSummary aggregatedResults) {
+        int numSubmitted = 0;
+        for (CCIProject project : projectList) {
+            Pattern projectNameFilterPattern = protexConfigManager
+                    .getProtexProjectNameFilterPattern();
+            if (projectNameFilterPattern != null) {
+                Matcher m = projectNameFilterPattern.matcher(project
+                        .getProjectName());
+                if (!m.matches()) {
+                    log.info(
+                            "Project {} does not match the project name filter; skipping it",
+                            project.getProjectName());
+                    CCIReportSummary skippedProjectResult = new CCIReportSummary();
+                    skippedProjectResult.addTotalProjectsSkipped();
+                    aggregatedResults.addReportSummary(skippedProjectResult);
+                    continue;
+                }
+            }
+            numSubmitted++;
+            Callable<CCIReportSummary> task = taskFactory.createTask(project);
+            completionService.submit(task);
+        }
+        return numSubmitted;
     }
 
     /**
@@ -218,11 +221,11 @@ public class CCISingleServerTaskProcessor extends CCIProcessor {
      * @return
      */
     public String getThreadExceptionMessages() {
-	return threadExceptionMessages.toString();
+        return threadExceptionMessages.toString();
     }
 
     private CCIProjectList getProjects() throws CodeCenterImportException {
-	return getProjects(protexServerWrapper);
+        return getProjects(protexServerWrapper);
     }
 
     /**
@@ -233,7 +236,7 @@ public class CCISingleServerTaskProcessor extends CCIProcessor {
     // TODO remove report functionality?
     @Override
     public void runReport() throws CodeCenterImportException {
-	throw new UnsupportedOperationException("runReport() not supported");
+        throw new UnsupportedOperationException("runReport() not supported");
     }
 
     /**
@@ -241,10 +244,10 @@ public class CCISingleServerTaskProcessor extends CCIProcessor {
      */
     @Override
     public CCIReportGenerator getReportGen() {
-	throw new UnsupportedOperationException("getReportGen() not supported");
+        throw new UnsupportedOperationException("getReportGen() not supported");
     }
 
     public CCIReportSummary getAggregatedResults() {
-	return aggregatedResults;
+        return aggregatedResults;
     }
 }
