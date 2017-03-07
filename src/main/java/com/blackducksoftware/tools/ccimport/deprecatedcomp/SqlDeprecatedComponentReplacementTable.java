e29161016a3aab155b5e79bf960895385283a72d
stevebillings <sbillings@blackducksoftware.com>
2017-03-06 19:32:21 -0500

    15: If db password is specified and encrypted: decrypt it

--- src/main/java/com/blackducksoftware/tools/ccimport/deprecatedcomp/SqlDeprecatedComponentReplacementTable.java
+++ src/main/java/com/blackducksoftware/tools/ccimport/deprecatedcomp/SqlDeprecatedComponentReplacementTable.java
@@ -15,6 +15,7 @@ import org.slf4j.LoggerFactory;
 
 import com.blackducksoftware.tools.ccimport.exception.CodeCenterImportException;
 import com.blackducksoftware.tools.commonframework.core.config.ConfigurationManager;
+import com.blackducksoftware.tools.commonframework.core.config.ConfigurationPassword;
 import com.blackducksoftware.tools.connector.protex.common.ComponentNameVersionIds;
 
 /**
@@ -31,17 +32,22 @@ public class SqlDeprecatedComponentReplacementTable implements DeprecatedCompone
 
     private final Logger log = LoggerFactory.getLogger(this.getClass().getName());
 
-    private Map<ComponentNameVersionIds, ComponentNameVersionIds> table = new HashMap<>(512);
+    private final Map<ComponentNameVersionIds, ComponentNameVersionIds> table = new HashMap<>(512);
 
-    public SqlDeprecatedComponentReplacementTable(ConfigurationManager config) throws CodeCenterImportException {
-        String dbServerName = config.getProperty("protex.db.server");
+    public SqlDeprecatedComponentReplacementTable(final ConfigurationManager config) throws CodeCenterImportException {
+        final String dbServerName = config.getProperty("protex.db.server");
         String dbUserName = config.getOptionalProperty("protex.db.user.name");
         if (dbUserName == null) {
             dbUserName = "blackduck";
         }
-        String dbPassword = config.getOptionalProperty("protex.db.password");
-        if (dbPassword == null) {
-            dbPassword = "mallard";
+
+        // Read the value of protex.db.password
+        String dbPassword = "mallard";
+        final ConfigurationPassword configurationPassword = ConfigurationPassword.createFromProperty(config.getProps(), "protex.db");
+        // test to see if the property was set or not
+        if (configurationPassword.getPlainText() != null) {
+            // get the plain text value of the password (even if it was encrypted in the property file)
+            dbPassword = configurationPassword.getPlainText();
         }
         String dbPort = config.getOptionalProperty("protex.db.port");
         if (dbPort == null) {
@@ -53,61 +59,62 @@ public class SqlDeprecatedComponentReplacementTable implements DeprecatedCompone
         }
         log.info("Reading database " + dbName + " for replacement tables (standard_project_release_deprecated, standard_project_deprecated)");
         try {
-            Connection conn = connectToDb(dbServerName, dbPort, dbName, dbUserName, dbPassword);
+            final Connection conn = connectToDb(dbServerName, dbPort, dbName, dbUserName, dbPassword);
             loadVersioned(conn, table);
             loadUnVersioned(conn, table);
             conn.close();
-        } catch (SQLException e) {
-            String msg = "Error loading replacement table from database " + dbName + " on " + dbServerName + ": " + e.getMessage();
+        } catch (final SQLException e) {
+            final String msg = "Error loading replacement table from database " + dbName + " on " + dbServerName + ": " + e.getMessage();
             log.error(msg);
             throw new CodeCenterImportException(msg);
         }
     }
 
-    private void loadVersioned(Connection conn, Map<ComponentNameVersionIds, ComponentNameVersionIds> table) throws SQLException {
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery(SQL_GET_VERSIONED);
+    private void loadVersioned(final Connection conn, final Map<ComponentNameVersionIds, ComponentNameVersionIds> table) throws SQLException {
+        final Statement stmt = conn.createStatement();
+        final ResultSet rs = stmt.executeQuery(SQL_GET_VERSIONED);
 
         while (rs.next()) {
-            String oldCompId = rs.getString("old_project_id");
-            String oldCompVersionId = rs.getString("old_release_id");
-            String newCompId = rs.getString("new_project_id");
-            String newCompVersionId = rs.getString("new_release_id");
+            final String oldCompId = rs.getString("old_project_id");
+            final String oldCompVersionId = rs.getString("old_release_id");
+            final String newCompId = rs.getString("new_project_id");
+            final String newCompVersionId = rs.getString("new_release_id");
             log.debug("Old Comp ID: " + oldCompId + ", OldCompVersionId: " + oldCompVersionId + ", New Comp ID: " + newCompId + ", New Comp Version ID: "
                     + newCompVersionId);
             addToTable(table, oldCompId, oldCompVersionId, newCompId, newCompVersionId);
         }
     }
 
-    private void loadUnVersioned(Connection conn, Map<ComponentNameVersionIds, ComponentNameVersionIds> table) throws SQLException {
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery(SQL_GET_UNVERSIONED);
+    private void loadUnVersioned(final Connection conn, final Map<ComponentNameVersionIds, ComponentNameVersionIds> table) throws SQLException {
+        final Statement stmt = conn.createStatement();
+        final ResultSet rs = stmt.executeQuery(SQL_GET_UNVERSIONED);
 
         while (rs.next()) {
-            String oldCompId = rs.getString("old_project_id");
-            String newCompId = rs.getString("new_project_id");
+            final String oldCompId = rs.getString("old_project_id");
+            final String newCompId = rs.getString("new_project_id");
             log.debug("Old Comp ID: " + oldCompId + ", New Comp ID: " + newCompId);
 
             addToTable(table, oldCompId, null, newCompId, null);
         }
     }
 
-    private void addToTable(Map<ComponentNameVersionIds, ComponentNameVersionIds> table, String oldCompId, String oldCompVersionId, String newCompId,
-            String newCompVersionId) {
-        ComponentNameVersionIds deprecatedComponent = new ComponentNameVersionIds(oldCompId, oldCompVersionId);
-        ComponentNameVersionIds replacementComponent = new ComponentNameVersionIds(newCompId, newCompVersionId);
+    private void addToTable(final Map<ComponentNameVersionIds, ComponentNameVersionIds> table, final String oldCompId, final String oldCompVersionId,
+            final String newCompId,
+            final String newCompVersionId) {
+        final ComponentNameVersionIds deprecatedComponent = new ComponentNameVersionIds(oldCompId, oldCompVersionId);
+        final ComponentNameVersionIds replacementComponent = new ComponentNameVersionIds(newCompId, newCompVersionId);
         table.put(deprecatedComponent, replacementComponent);
     }
 
     @Override
-    public ComponentNameVersionIds getReplacement(ComponentNameVersionIds deprecatedComponent) {
+    public ComponentNameVersionIds getReplacement(final ComponentNameVersionIds deprecatedComponent) {
         return table.get(deprecatedComponent);
     }
 
-    private Connection connectToDb(String dbServerName, String dbPort, String dbName, String dbUserName, String dbPassword)
+    private Connection connectToDb(final String dbServerName, final String dbPort, final String dbName, final String dbUserName, final String dbPassword)
             throws SQLException {
-        String url = "jdbc:postgresql://" + dbServerName + ":" + dbPort + "/" + dbName;
-        Properties dbProps = new Properties();
+        final String url = "jdbc:postgresql://" + dbServerName + ":" + dbPort + "/" + dbName;
+        final Properties dbProps = new Properties();
         dbProps.setProperty("user", dbUserName);
         dbProps.setProperty("password", dbPassword);
         log.info("Connecting to: " + url);
